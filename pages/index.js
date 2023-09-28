dab1a40531bc0e0ac7c84769c5486e5474362c0d
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-09-28 03:14:23 +0000

    adicionado `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-  return <h1>Clone do tabnews</h1>
+  return <h1>Clone do tabnews</h1>;
 }
 
-export default Home
+export default Home;
