06d6d3de0b0a0f430731dd9867af6bcb15bf6a70
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-11-17 10:34:30 -0400

    adicionado `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
-import database from "../../../../infra/database";
-// console.log()
+import database from "infra/database";
+
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows)
