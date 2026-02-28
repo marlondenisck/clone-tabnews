84f3195868789f96fa374c0a97e41b540de48e10
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-02-28 15:08:21 -0400

    refactor: substitui Client por Pool para gerenciamento de conexões com o banco de dados

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,37 @@
-import { Client } from 'pg'
+// import { Client } from 'pg'
+
+// async function query(queryObject) {
+//   const client = new Client({
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     user: process.env.POSTGRES_USER,
+//     database: process.env.POSTGRES_DB,
+//     password: process.env.POSTGRES_PASSWORD,
+//   })
+//   await client.connect()
+//   const result = await client.query(queryObject)
+//   await client.end()
+//   return result
+// }
+
+// export default {
+//   query: query,
+// }
+
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+  return await pool.query(queryObject);
 }
-
 
 export default {
   query: query,
-}
+};
