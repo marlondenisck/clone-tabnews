4607d2eea3ff7d8f25be8183175d09501a0c3d09
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-03-07 18:14:51 -0400

    test com logs

--- infra/database.js
+++ infra/database.js
@@ -1,37 +1,51 @@
-// import { Client } from 'pg'
-
-// async function query(queryObject) {
-//   const client = new Client({
-//     host: process.env.POSTGRES_HOST,
-//     port: process.env.POSTGRES_PORT,
-//     user: process.env.POSTGRES_USER,
-//     database: process.env.POSTGRES_DB,
-//     password: process.env.POSTGRES_PASSWORD,
-//   })
-//   await client.connect()
-//   const result = await client.query(queryObject)
-//   await client.end()
-//   return result
-// }
-
-// export default {
-//   query: query,
-// }
-
-import { Pool } from "pg";
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-});
+import { Client } from "pg";
 
 async function query(queryObject) {
-  return await pool.query(queryObject);
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
   query: query,
 };
+
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST,
+//   port: process.env.POSTGRES_PORT,
+//   user: process.env.POSTGRES_USER,
+//   database: process.env.POSTGRES_DB,
+//   password: process.env.POSTGRES_PASSWORD,
+// });
+
+// async function query(queryObject) {
+//   return await pool.query(queryObject);
+// }
+
+// export default {
+//   query: query,
+// };
