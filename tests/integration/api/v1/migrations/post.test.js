89464a1b2e61c2b5094e7f42f127e24ea9488934
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-04-18 15:26:04 -0400

    refact: melhora a legibilidade dos testes e atualiza scripts no package.json

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,39 +6,42 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-describe("POST to /api/v1/migrations", () => {
-  test("deve retornar status 200", async () => {
-    const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Rodar migrations pendentes", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+      });
+
+      expect(response1.status).toBe(201);
+
+      const contentType = response1.headers.get("content-type");
+      expect(contentType).toContain("application/json");
+
+      const responseBody = await response1.json();
+
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+
+      responseBody.forEach((migration) => {
+        expect(migration).toEqual(
+          expect.objectContaining({
+            path: expect.any(String),
+            name: expect.any(String),
+            timestamp: expect.any(Number),
+          }),
+        );
+      });
     });
+    test("Não deve rodar migrations novamente se já estiverem rodadas", async () => {
+      const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+      });
 
-    expect(response1.status).toBe(201);
-
-    const contentType = response1.headers.get("content-type");
-    expect(contentType).toContain("application/json");
-
-    const responseBody = await response1.json();
-
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
-
-    responseBody.forEach((migration) => {
-      expect(migration).toEqual(
-        expect.objectContaining({
-          path: expect.any(String),
-          name: expect.any(String),
-          timestamp: expect.any(Number),
-        }),
-      );
+      expect(response2.status).toBe(200);
+      const response2Body = await response2.json();
+      expect(Array.isArray(response2Body)).toBe(true);
+      expect(response2Body.length).toBe(0);
     });
-
-    const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
-    });
-
-    expect(response2.status).toBe(200);
-    const response2Body = await response2.json();
-    expect(Array.isArray(response2Body)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
   });
 });
