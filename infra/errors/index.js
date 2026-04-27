ae046020d8402763b5cca4a091e139b0696876c3
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-04-27 11:30:37 -0400

    feat: adiciona tratamento de erro para método não permitido e atualiza a estrutura de erro

--- infra/errors/index.js
+++ infra/errors/index.js
@@ -11,7 +11,26 @@ export class InternalServerError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
-      statusCode: this.statusCode,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é valido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
     };
   }
 }
