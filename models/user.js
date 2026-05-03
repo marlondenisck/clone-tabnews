aacc9762318e8a9bea688da69ebfe46d26d3ef08
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-05-02 20:14:11 -0400

    feat: adiciona tratamento de erro NotFoundError e implementa busca de usuário por username

--- models/user.js
+++ models/user.js
@@ -1,5 +1,35 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM 
+          users 
+        WHERE 
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+      `,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "Usuário não encontrado.",
+        action: "Verifique o username informado e tente novamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -72,6 +102,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
