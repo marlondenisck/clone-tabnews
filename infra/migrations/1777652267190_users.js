c94426b5178f5da3caad2b65461370cfbefb67f3
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-05-03 19:13:07 -0400

    feat: ajusta tipo de dado da senha para varchar(60) e atualiza a função de timestamp para timezone UTC

--- infra/migrations/1777652267190_users.js
+++ infra/migrations/1777652267190_users.js
@@ -18,8 +18,10 @@ export const up = (pgm) => {
       unique: true,
     },
     // https://security.stackexchange.com/a/39851
+    // o input deve ser 72 mas o hash gerado é de sempre de 60 caracteres
+    // https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
     // https://justatheory.com/2012/04/postgres-use-timestamptz/
@@ -27,11 +29,13 @@ export const up = (pgm) => {
       // type: "timestamp with time zone", é equivalente a "timestamptz", ambos são tipos de dados que armazenam data e hora com fuso horário. A escolha entre eles é principalmente uma questão de estilo e preferência pessoal, pois ambos são amplamente suportados e funcionam de maneira semelhante.
       type: "timestamptz",
       // "CURRENT_TIMESTAMP" é equivalente a "now()", ambos retornam a data e hora atual do servidor. A escolha entre eles é principalmente uma questão de estilo e preferência pessoal, pois ambos são amplamente suportados e funcionam de maneira semelhante.
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
     },
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
     },
   });
 };
