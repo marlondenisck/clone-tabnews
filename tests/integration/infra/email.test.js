2d0e15e38a96f816108a40f5b779c041a6e8e8a9
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2026-05-16 13:57:34 -0400

    feat: adiciona suporte para verificação e exclusão de emails no orquestrador de testes

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,12 +1,32 @@
 import email from "@/infra/email";
+import orchestrator from "@/tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.deleteAllEmails();
+});
 
 describe("infra/email", () => {
-  it("send email", async () => {
-    const result = await email.send({
+  test("send email", async () => {
+    await email.send({
       from: "Remetente <remetente@example.com>",
       to: "Destinatário <destinatario@example.com>",
       subject: "Assunto do email",
       text: "Corpo do email",
     });
+
+    await email.send({
+      from: "Remetente <remetente@example.com>",
+      to: "Destinatário <destinatario@example.com>",
+      subject: "Ultimo email enviado",
+      text: "Corpo do ultimo email",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+    // console.log("lastEmail", lastEmail);
+    expect(lastEmail.sender).toBe("<remetente@example.com>");
+    expect(lastEmail.recipients[0]).toBe("<destinatario@example.com>");
+    expect(lastEmail.subject).toBe("Ultimo email enviado");
+    expect(lastEmail.text.trim()).toBe("Corpo do ultimo email");
   });
 });
